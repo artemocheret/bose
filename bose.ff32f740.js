322c15062e047764506f5445bfd793ad99a5fe73
artemocheret <artemoceret24@gmail.com>
2025-11-27 16:20:58 +0000

    Deploying to gh-pages from @ artemocheret/bose@37e17470185dc13da23fad53987ad8cc1a46a472 🚀

--- bose.ff32f740.js
+++
@@ -1,2 +0,0 @@
-let e=document.querySelector("[data-menu]"),t=document.querySelector("[data-menu-btn]");document.querySelectorAll("[data-mobile-link]").forEach(s=>{s.addEventListener("click",()=>{e.classList.remove("is-open"),t.classList.remove("is-open")})}),t.addEventListener("click",()=>{e.classList.toggle("is-open"),t.classList.toggle("is-open")});
-//# sourceMappingURL=bose.ff32f740.js.map
